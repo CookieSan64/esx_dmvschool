c7ac50e9dad0bb64b50d242afa6dba3852db25a3
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:08:58 +0100

    Initial commit

---
+++ html/questions.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+    {
+        question: "Si vous allez à 80 km/h et vous approchez d'une zone résidentielle, que devez-vous faire ?",
+        propositionA: "Vous accélérez.",
+        propositionB: "Vous gardez votre vitesse s'il n'y a pas d'autres voitures.",
+        propositionC: "Vous ralentissez.",
+        propositionD: "Vous gardez votre vitesse.",
+        reponse: "C"
+    },
+
+    {
+        question: "Si vous tournez à droite à un feu et voyez quelqu'un traverser, que faites-vous ?",
+        propositionA: "Vous contournez le piéton.",
+        propositionB: "Vous vérifiez qu'il n'y a plus de véhicules aux alentours.",
+        propositionC: "Vous attendez que le piéton ait traversé.",
+        propositionD: "Vous roulez sur le piéton et continuez à conduire.",
+        reponse: "C"
+    },
+
+    {
+        question: "Sans indication spécifique, quelle est la vitesse permise dans une zone résidentielle ?",
+        propositionA: "30 km/h.",
+        propositionB: "50 km/h.",
+        propositionC: "40 km/h.",
+        propositionD: "60 km/h.",
+        reponse: "B"
+    },
+
+    {
+        question: "Avant chaque changement de voie, que devez-vous faire ?",
+        propositionA: "Regarder vos rétroviseurs.",
+        propositionB: "Vérifier vos angles morts.",
+        propositionC: "Signaler vos intentions.",
+        propositionD: "Toutes ces réponses.",
+        reponse: "D"
+    },
+
+    {
+        question: "À quel taux d’alcoolémie est-il considéré comme conduite en état d'ivresse ?",
+        propositionA: "0,05%.",
+        propositionB: "0,18%.",
+        propositionC: "0,08%.",
+        propositionD: "0,06%.",
+        reponse: "C"
+    },
+
+    {
+        question: "Quand pouvez-vous continuer à conduire à un feu de signalisation ?",
+        propositionA: "Lorsqu'il est vert.",
+        propositionB: "Lorsqu'il n'y a personne à l'intersection.",
+        propositionC: "Lorsque vous êtes dans une zone scolaire.",
+        propositionD: "Lorsqu'il est vert et/ou rouge et que vous tournez à droite.",
+        reponse: "D"
+    },
+
+    {
+        question: "Si un piéton a un signal lui indiquant de ne pas traverser, que faites-vous ?",
+        propositionA: "Vous le laissez passer.",
+        propositionB: "Vous observez avant de continuer.",
+        propositionC: "Vous lui faites signe de traverser.",
+        propositionD: "Vous continuez car le feu est vert.",
+        reponse: "D"
+    },
+
+    {
+        question: "Que devez-vous éviter de faire lorsque vous êtes dépassé par un autre véhicule ?",
+        propositionA: "Ralentir.",
+        propositionB: "Vérifier vos rétroviseurs.",
+        propositionC: "Regarder les autres conducteurs.",
+        propositionD: "Augmenter votre vitesse.",
+        reponse: "D"
+    },
+
+    {
+        question: "Sur une autoroute avec une vitesse maximale de 120 km/h, si la plupart des conducteurs conduisent à 125 km/h, quelle vitesse maximale devriez-vous respecter ?",
+        propositionA: "120 km/h.",
+        propositionB: "125 km/h.",
+        propositionC: "130 km/h.",
+        propositionD: "110 km/h.",
+        reponse: "A"
+    },
+
+    {
+        question: "Lorsque vous êtes dépassé par un autre véhicule, il est important de NE PAS :",
+        propositionA: "Ralentir.",
+        propositionB: "Vérifier vos rétroviseurs.",
+        propositionC: "Regarder les autres conducteurs.",
+        propositionD: "Augmenter votre vitesse.",
+        reponse: "D"
+    },
+]
